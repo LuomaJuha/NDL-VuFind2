30109d6472cc519c84e2b3cbc4773624d7b85ff8
Juha Luoma <juhaluoma5@gmail.com>
2025-12-31 09:31:16 +0200

    very raw version of lightbox popup

--- module/Finna/config/module.config.php
+++ module/Finna/config/module.config.php
@@ -1260,5 +1260,18 @@ $config['router']['routes']['solrauthrecord-feedback'] = [
         ],
     ],
 ];
-
+$config['router']['routes']['record-modal'] = [
+    'type'    => 'Laminas\Router\Http\Segment',
+    'options' => [
+        'route'    => '/Record/[:id]/Modal',
+        'constraints' => [
+            'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+            'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+        ],
+        'defaults' => [
+            'controller' => 'Record',
+            'action'     => 'Modal',
+        ],
+    ],
+];
 return $config;
